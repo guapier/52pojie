15e0eb98b764f54120d5d21144f7e3d22f5dd890
guapier <451875565@qq.com>
2019-02-22 11:36:12 +0800

    delete the unuse package

--- 52.py
+++ 52.py
@@ -5,14 +5,8 @@
 # @Function:
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from io import BytesIO
-import re
-import random, time
-import shutil
+import time
 import base64
 import apiutil
 import random
